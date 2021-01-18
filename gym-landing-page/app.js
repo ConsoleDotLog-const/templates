aae7df698dc18932f0c092e478163667ddc69328
ConsoleDotLog-const <c-gusto@hotmail.com>
2021-01-18 17:20:42 +1300

    navbar

---
+++ gym-landing-page/app.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+const navLogo = document.querySelector('#navbar__logo')
+const body = document.querySelector('body')
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('is-active')
+    body.classList.toggle('is-active')
+}
+
+menu.addEventListener('click', mobileMenu)
